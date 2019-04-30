3d9abcccfa2152d944cbb86e2f7272e1bbab6532
Pingchuan Ma <pika7ma@gmail.com>
2019-04-30 22:17:09 +0800

    update

--- hogwarts/distributed/misc.py
+++ hogwarts/distributed/misc.py
@@ -16,12 +16,9 @@ def _check_tensor_list(tensor_list):
 
 
 def _get_host_ip():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect(('8.8.8.8', 80))
         ip = s.getsockname()[0]
-    finally:
-        s.close()
     return ip
 
 
