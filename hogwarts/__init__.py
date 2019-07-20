3d84f6c51ec70db03afadc683adf0124f944a4f3
Pingchuan Ma <mapingchuan.mit@bytedance.com>
2019-07-20 23:54:02 +0800

    fix bug in __init__

--- hogwarts/__init__.py
+++ hogwarts/__init__.py
@@ -1,6 +1,10 @@
 from . import utils
 from . import command
 from . import record
+from . import metrics
+from . import lr_schedulers
+from . import data
+from . import distributed
 
 from .logger import log
 from .config import Config
